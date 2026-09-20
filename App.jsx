276966afb80ba3f7ecdf636d37f9b75fb43321b5
Jys_vGH <aljromev@gmail.com>
2026-09-21 03:26:44 +0800

    update page styling

--- App.jsx
+++ App.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import Footer from "./components/Footer";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Footer from "./Footer";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        {" | "}
-        <Link to="/about">About</Link>
+      <nav className="navbar">
+        <div className="nav-logo">My Website</div>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
       </nav>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </main>
 
       <Footer />
     </BrowserRouter>
