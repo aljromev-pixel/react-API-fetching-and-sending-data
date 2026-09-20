319ecbd4897dcc7e93258bdc13d29db8b186b64f
Jys_vGH <aljromev@gmail.com>
2026-09-21 03:26:44 +0800

    refine page components

--- Homepage.jsx
+++ Homepage.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div>
-      <h1>CCS-112 React Project!</h1>
-      <p>Welcome to my Homepage.</p>
+    <div className="homepage">
+      <div className="hero">
+        <h1>CCS-112 React Project!</h1>
+        <p>Welcome to my Homepage.</p>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>CCS-112 React Project!</h1>
+      <p>Welcome to my Homepage.</p>
+    </div>
+  );
+}
+
+export default HomePage;
